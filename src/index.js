540891e42db30b58224a6c8234d9ae6a4924e63a
BegenchGurbanmammedov <93909709+BegenchGurbanmammedov@users.noreply.github.com>
2023-02-08 17:49:45 +0500

    added redux-persist to project
    redux-persist saving store to localStorage

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
-import { store } from "./store/store";
+import { store,persistor } from "./store/store";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store= {store}>
-      <BrowserRouter>
-
-     
-      
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
             <App />
-      
-
-    </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-    
   </React.StrictMode>
 );
 
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,22 +1,33 @@
 import { compose,legacy_createStore,applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer'; 
+import { persistStore, persistReducer } from 'redux-persist';
+import storage  from 'redux-persist/lib/storage';
 
+const persistConfig = {
+    key:'root',
+    storage,
+    blacklist:['user']
 
-
-const loggerMiddleware = (store)=>(next)=>(action)=>{
-    if(!action.type){
-        return next(action);
-    }
-    // console.log('type', action.type);
-    // console.log('payload: ',action.payload);
-    console.log('curretState',store.getState())
-    next(action);
-
-    console.log( 'next state: ', store.getState() )
 }
-const middleWares = [loggerMiddleware]
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+//shuny papka gechirmeli middlewares diyen. ozun doredip
+// const loggerMiddleware = (store)=>(next)=>(action)=>{
+//     if(!action.type){
+//         return next(action);
+//     }
+//     // console.log('type', action.type);
+//     // console.log('payload: ',action.payload);
+//     console.log('curretState',store.getState())
+//     next(action);
+
+//     console.log( 'next state: ', store.getState() )
+// }
+const middleWares = [logger]
 // const middleWares = [logger]
 const composedEnhancers = compose(applyMiddleware(...middleWares))
 
-export const store = legacy_createStore(rootReducer,undefined,composedEnhancers)
+export const store = legacy_createStore(persistedReducer,undefined,composedEnhancers)
+
+export const persistor = persistStore(store);
